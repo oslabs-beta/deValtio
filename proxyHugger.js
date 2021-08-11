e4f0adc6537b227e69530c84c73a41355ec0b131
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-08-10 21:06:38 -0400

    Added apply listener and set up a pointer to the original Proxy object via origProxy.

--- proxyHugger.js
+++ proxyHugger.js
@@ -2,9 +2,11 @@ console.log(`proxyHugger.js is running`);
 
 const pp = (stuff) => JSON.stringify(stuff, null, 2) // Pretty Print
 
+let counter = 0;
+
 const proxyViewer = {
   get: function (target, prop, receiver) {
-    console.log(`get called with: target: ${pp(target)}, prop: ${pp(prop)}, receiver: ${pp(receiver)}`);
+    console.log(`get called with: target: ${pp(target.name)}, prop: ${pp(prop)}, receiver: ${pp(receiver)}`);
     return Reflect.get(...arguments);
   },
   set: function(obj, prop, value) {
@@ -12,9 +14,15 @@ const proxyViewer = {
 	return Reflect.get(...arguments);
   },
   construct: function(target, args) {
-	  console.log(`construct called with: target: ${pp(target)}, ${pp(args)}`);
+	  console.log(`construct called with: target: ${pp(target.name)}, ${pp(args)}`);
+    console.log(`has ${Object.getOwnPropertyNames(target)}`);
 	  return Reflect.construct(target, args);
   },
+  apply: function(target, thisArg, argumentsList) {
+    console.log(`apply called with: ${pp(target.name)}, thisArg: ${pp(thisArg)}, argumentsList: ${pp(argumentsList)}`)
+    return Reflect.apply(target, thisArg, argumentsList);
+  }
 };
 
-Proxy = new Proxy(Proxy, proxyViewer);
+const origProxy = Proxy;
+Proxy = new origProxy(origProxy, proxyViewer);

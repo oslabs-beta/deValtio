3ebc6b8d6de53076cf8f84b325c7b394f661a18c
Gordon Hui <randomizer@macbook-pro.lan>
2021-08-23 23:06:33 -0400

    renders diff, added 3 npm packages

--- src/frontend/Components/StateDiff/statediff.tsx
+++ src/frontend/Components/StateDiff/statediff.tsx
@@ -1,15 +1,35 @@
 //this is for state drawn out 
+import {diff, formatters} from "jsondiffpatch";
+import ReactHtmlParser from 'react-html-parser';
 
 
 function StateDiff(): JSX.Element {
 
+  let dummyone = {
+    firstname: "hello",
+    lastname: "bye"
+  }
+  let dummytwo = {
+    firstname: "bye",
+    lastname: "hello"
+  }
+
   //extract intial state from the app and display it as an object 
   //as we add to state show the updates to state - save it as a snapshot and display it in the snapshot section 
   //everytime state is update, it should display updated state and save it as a new snapshot 
+  //this is for diffing difference between 2 snapshots
+const delta: any = diff(dummyone, dummytwo);
+console.log('this is the delta version', delta);
+//this is to covert difference to html format
+const html = formatters.html.format(delta, dummyone);
+console.log('this is the formatted version', html)
+
 
   return (
     <div className='Diff'>
-      render the states here
+      changes here will render what I need
+      this turns the html back into react code to render
+      {ReactHtmlParser(html)}
     </div>
   )
 }

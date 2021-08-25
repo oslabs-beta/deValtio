b47d337d0b98a6650344e7285f852abc6af2c331
Gordon Hui <randomizer@macbook-pro.lan>
2021-08-24 22:26:24 -0400

    added mock data in another file and made sure able to render

--- src/frontend/Components/StateDiff/statediff.tsx
+++ src/frontend/Components/StateDiff/statediff.tsx
@@ -1,27 +1,28 @@
 //this is for state drawn out 
 import {diff, formatters} from "jsondiffpatch";
 import ReactHtmlParser from 'react-html-parser';
+import {curSnapMock, prevSnapMock, snapshotTestArray} from './mockStateDiff'
 
 
 function StateDiff(): JSX.Element {
 
-  let dummyone = {
-    firstname: "hello",
-    lastname: "bye"
-  }
-  let dummytwo = {
-    firstname: "bye",
-    lastname: "hello"
-  }
+  // let dummyone = [{
+  //   firstname: "hello",
+  //   lastname: "bye"
+  // }]
+  // let dummytwo = [{
+  //   firstname: "bye",
+  //   lastname: "hello"
+  // }]
 
   //extract intial state from the app and display it as an object 
   //as we add to state show the updates to state - save it as a snapshot and display it in the snapshot section 
   //everytime state is update, it should display updated state and save it as a new snapshot 
   //this is for diffing difference between 2 snapshots
-const delta: any = diff(dummyone, dummytwo);
+const delta: any = diff(curSnapMock, prevSnapMock);
 console.log('this is the delta version', delta);
 //this is to covert difference to html format
-const html = formatters.html.format(delta, dummyone);
+const html = formatters.html.format(delta, prevSnapMock);
 console.log('this is the formatted version', html)
 
 

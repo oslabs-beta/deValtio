0eabbb74aba64480ad96ef00edb8403ae7ac7e80
Gordon Hui <randomizer@macbook-pro.lan>
2021-08-28 12:21:30 -0400

    changed the dummy data

--- src/frontend/Components/StateDiff/statediff.tsx
+++ src/frontend/Components/StateDiff/statediff.tsx
@@ -1,6 +1,6 @@
 import {diff, formatters} from "jsondiffpatch";
 import ReactHtmlParser from 'react-html-parser';
-import {curSnapMock, prevSnapMock, snapshotTestArray} from './mockStateDiff'
+//import {curSnapMock, prevSnapMock, snapshotTestArray} from './mockStateDiff'
 import styled from "styled-components";
 
 
@@ -14,23 +14,23 @@ function StateDiff(): JSX.Element {
   `
 
 
-  // let dummyone = {
-  //   firstname: "hello",
-  //   lastname: "bye"
-  // }
-  // let dummytwo = {
-  //   firstname: "bye",
-  //   lastname: "hello"
-  // }
+  let dummyone = {
+    firstname: "hello",
+    lastname: "bye"
+  }
+  let dummytwo = {
+    firstname: "bye",
+    lastname: "hello"
+  }
 
   //extract intial state from the app and display it as an object 
   //as we add to state show the updates to state - save it as a snapshot and display it in the snapshot section 
   //everytime state is update, it should display updated state and save it as a new snapshot 
   //this is for diffing difference between 2 snapshots
-const delta: any = diff(curSnapMock, prevSnapMock);
+const delta: any = diff(dummyone, dummytwo);
 console.log('this is the delta version', delta);
 //this is to covert difference to html format
-const html = formatters.html.format(delta, prevSnapMock);
+const html = formatters.html.format(delta, dummyone);
 console.log('this is the formatted version', html)
 
 

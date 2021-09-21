19a3059a617939f4318235d10146f2d0b181ecf2
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-09-20 23:24:45 -0400

    Proxied the proxy object. Target objects are in "args" but it seems there are at least two objects in there

--- injected.js
+++ injected.js
@@ -1,7 +1,38 @@
-console.log(`injected.js has been initiated`)
+console.log(`injected.js has been initiated`);
+
+const pp = stuff => JSON.stringify(stuff, null, 2);
+
+const origProxy = Proxy;
+
+const proxyHandler = {
+  construct: (target, args, receiver) => {
+    let caller;
+    
+    try {
+      throw new Error();
+    } catch(err) {
+      console.log(`err.stack is ${pp(err.stack)}`);
+      caller = err.stack.split("at ")[2].split(" (")[0];
+    }
+    
+    console.log(`Proxy caller is: ${caller} and params are ${pp(args)}`)
+    // return new origProxy(target, args, receiver);
+    return Reflect.construct(target, args, receiver);
+  }
+};
+
+Proxy = new origProxy(origProxy, proxyHandler);
+
+// Proxy = (target, args, receiver) => {
+//   console.log(`target: ${target}, args: ${args}, receiver: ${receiver}`);
+// Proxy = () => {
+//   console.log(`params are: ${arguments}`);
+//   // return new origProxy(arguments);
+// };
+// Proxy.prototype = origProxy.prototype;
 
 document.onreadystatechange = () => {
-  if (document.readyState === 'interactive' || document.readyState === 'complete') {
+  if (document.readyState === 'complete') {
 
     const reactRoots = [];
     document.querySelectorAll('*').forEach(node => {

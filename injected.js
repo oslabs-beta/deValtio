cb0fbac3e3db48beabada1997185252f2db1713f
Sergey01 <Sergey01@users.noreply.github.com>
2021-09-21 19:56:59 -0400

    Working on proxying the state object in injected.js. What's interesting is the second item of the args array passed to the proxyHandler construct function.

--- injected.js
+++ injected.js
@@ -4,6 +4,13 @@ const pp = stuff => JSON.stringify(stuff, null, 2);
 
 const origProxy = Proxy;
 
+const objectHandler = {
+  get: (target, prop, receiver) => {
+    console.log(`target: ${target}, prop: ${prop}, receiver: ${receiver}`);
+    Reflect.get(...arguments);
+  }
+};
+
 const proxyHandler = {
   construct: (target, args, receiver) => {
     let caller;
@@ -11,12 +18,20 @@ const proxyHandler = {
     try {
       throw new Error();
     } catch(err) {
-      console.log(`err.stack is ${pp(err.stack)}`);
+      // console.log(`err.stack is ${pp(err.stack)}`);
       caller = err.stack.split("at ")[2].split(" (")[0];
     }
     
-    console.log(`Proxy caller is: ${caller} and params are ${pp(args)}`)
-    // return new origProxy(target, args, receiver);
+    // console.log(`Proxy caller is: ${caller} and params are ${pp(args)}`)
+    console.dir(Reflect.ownKeys(args[1]));
+    console.dir(args[1]['f'])
+    console.dir(args[1].constructor)
+    // console.log(Reflect.ownKeys(args[-1]));
+    // args.forEach((arg) => {
+    //   console.log(`arg is ${arg}`);
+    //   arg = new Proxy(arg, objectHandler);
+    // });
+    // // return new origProxy(target, args, receiver);
     return Reflect.construct(target, args, receiver);
   }
 };

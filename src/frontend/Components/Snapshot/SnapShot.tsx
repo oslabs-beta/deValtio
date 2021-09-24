70d7bab25475048100c27d57075763be771800d0
Gordon Hui <randomizer@macbook-pro.lan>
2021-09-23 22:51:23 -0400

    styling for snapshot buttons and state diff changes"
    q

--- src/frontend/Components/Snapshot/SnapShot.tsx
+++ src/frontend/Components/Snapshot/SnapShot.tsx
@@ -6,20 +6,25 @@ const SnapShotButton = styled.button<{ snapShotIndex: number }>`
     background: ${props => props.value === props.snapShotIndex ? '#E0FBFC' : '#98C1D9'};
     color: ${props => props.value === props.snapShotIndex ? '#98C1D9' : '#E0FBFC'};
     width: 50%;
+    min-width: 50%
     height: 2em;
+    min-height: 50%:
     border: 1px solid #98C1D9;
     border-radius: 2px;
     margin: 7px 4px;
     cursor: pointer;
+    font-size: 100%;
+    overflow-wrap: break-word;
     &:hover {
         color: #293241; 
+    
     };
 `;
 
-const P = styled.p`
-    margin: 0.3em;
-    font-size: 1.2em;
-`;
+// const P = styled.p`
+//     margin: 0.3em;
+//     font-size: 1.2em;
+// `;
 
 export const SnapShot = ({ snapNum, setSnapShotIndex, value }: { snapNum: number, setSnapShotIndex: Dispatch<SetStateAction<number>>, value: number }) => {
 

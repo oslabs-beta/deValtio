4d9114437d00b125c3e479349c2f3501bd56b421
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2022-02-13 23:47:09 -0500

    Added eslint and prettier with AirBnB style guide

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
